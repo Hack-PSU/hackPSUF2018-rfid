ad13d4685844d1ad38950eafe438cf9160e8e513
jmm8046 <35675433+jmm8046@users.noreply.github.com>
2018-06-08 01:34:42 -0400

    Add files via upload (#3)
    Added prototypes for HTTP lib

---
+++ sketch/hackPSUhttp.cpp
@@ -0,0 +1,25 @@
+#include <hackPSUhttp.h>
+
+namespace hackPSU {
+	Response HTTP::GET(String url) {
+	    HTTPClient http;
+	    Response response;
+	    http.begin(url);
+	    int httpCode = http.GET();
+		
+	    //httpCode will be negative (or zero) if there is an error
+	    if (httpCode > 0){ 	//If the GET call was successful
+		    response.payload = http.getString(); 
+		    response.responseCode = httpCode;
+	    } else {
+		    response.payload = http.errorToString(httpCode).c_str();
+		    response.responseCode = httpCode;
+	    }
+	http.end();
+	return responseInfo;
+	};
+}
+
+
+
+
---
+++ sketch/hackPSUhttp.h
@@ -0,0 +1,29 @@
+#pragma once 
+
+#include <stdint.h>
+#include <stdlib.h>
+#include <HttpClient.h>
+
+namespace hackPSU {
+	
+	struct Response {
+  		String payload;
+  		int responseCode;
+	}responseInfo;   //declare structure obejct to return
+
+  class HTTP{
+    public:
+	  /**
+	   * TODO: Add docstring explaining these parameters
+	   */
+      static Response GET(String url, String headers)
+      static Response GET(String url)
+      static Response POST(String url, String payload, String headers)
+      static Response GET(String url, String payload)
+
+  };
+}
+
+
+
+
