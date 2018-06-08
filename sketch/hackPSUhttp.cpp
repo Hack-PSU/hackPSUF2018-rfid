6b724672b7bc688e756d28744bbe6183bdfee78a
Sushrut Shringarputale <sps5394@psu.edu>
2018-06-07 22:31:29 -0700

    Code formatting changes

--- sketch/hackPSUhttp.cpp
+++ sketch/hackPSUhttp.cpp
@@ -1,31 +1,23 @@
-#pragma once 
-
-#include <stdint.h>
-#include <stdlib.h>
-#include <HttpClient.h>
+#include <hackPSUhttp.h>
 
 namespace hackPSU {
-	
-	struct Response{
-  		String payload;
-  		int responseCode;
-	}responseInfo;   //declare structure obejct to return
-
-  class HTTP{
-    HTTPClient http;
-    http.begin("I DONT KNOW THE IP"); //http Begin call
-    int httpCode = http.GET(); //GET call
-
-  	//httpCode will be negative (or zero) if there is an error
-  	if (httpCode > 0){ 	//If the GET call was successful
-      responseInfo.payload = http.getString(); 		//getting the repsonse from the get
-      responseInfo.responseCode = httpCode;
-  	}else{
-      responseInfo.payload = http.errorToString(httpCode).c_str(); //I think this is the error information??
-      responseInfo.responseCode = httpCode; //should be negative or zero?
-  	}
-    http.end();
-  };
+	Response HTTP::GET(String url) {
+	    HTTPClient http;
+	    Response response;
+	    http.begin(url);
+	    int httpCode = http.GET();
+		
+	    //httpCode will be negative (or zero) if there is an error
+	    if (httpCode > 0){ 	//If the GET call was successful
+		    response.payload = http.getString(); 
+		    response.responseCode = httpCode;
+	    } else {
+		    response.payload = http.errorToString(httpCode).c_str();
+		    response.responseCode = httpCode;
+	    }
+	http.end();
+	return responseInfo;
+	};
 }
 
 
