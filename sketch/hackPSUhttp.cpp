7598503a47bf129e6c8c36ce2a35a0138d9e90ed
jmm8046 <35675433+jmm8046@users.noreply.github.com>
2018-07-12 14:22:30 -0400

    Update hackPSUhttp.cpp

--- sketch/hackPSUhttp.cpp
+++ sketch/hackPSUhttp.cpp
@@ -1,36 +1,36 @@
+//https://techtutorialsx.com/2016/07/21/esp8266-post-requests/
 #include <hackPSUhttp.h>
 
 namespace hackPSU {
   	Response::GET(String url, String payload, struct headers){
   		HTTPClient http;
    		http.begin(url); //http Begin call
-    		int httpCode = http.GET(); //GET call
-
     		http.addHeader(headerInfo.header);
+		int httpCode = http.GET(); //GET call
 
   		//httpCode will be negative (or zero) if there is an error
+
   		if (httpCode > 0){ 	//If the GET call was successful
       			responseInfo.payload = http.getString(); 		//getting the repsonse from the get
       			responseInfo.responseCode = httpCode;
   		}else{
-     			responseInfo.payload = http.errorToString(httpCode).c_str(); //I think this is the error information??
+     			responseInfo.payload = http.errorToString(httpCode); //I think this is the error information??
      			responseInfo.responseCode = httpCode; //should be negative or zero?
-  			}
+  		}
     		http.end();
   	}
-	
+
   	Response::GET(String url, String payload){
     		HTTPClient http;
     		http.begin(url); //http Begin call
    		int httpCode = http.GET(); //GET call
-    		http.addHeader(headerInfo.header);
 
   		//httpCode will be negative (or zero) if there is an error
   		if (httpCode > 0){ 	//If the GET call was successful
      			responseInfo.payload = http.getString(); 		//getting the repsonse from the get
       			responseInfo.responseCode = httpCode;
   		}else{
-      			responseInfo.payload = http.errorToString(httpCode).c_str(); //I think this is the error information??
+      			responseInfo.payload = http.errorToString(httpCode); //I think this is the error information??
       			responseInfo.responseCode = httpCode; //should be negative or zero?
   		}
     		http.end();
@@ -39,16 +39,17 @@ namespace hackPSU {
   	Response::POST(String url, String payload, struct headers){
     		HTTPClient http;
     		http.begin(url); //http Begin call
-    		int httpCode = http.GET(); //GET call
     		http.addHeader(headerInfo.header);
 
+    		int httpCode = http.POST(payload); //POST call sending the payload?
+
   		//httpCode will be negative (or zero) if there is an error
-  		if (httpCode > 0){ 	//If the GET call was successful
-      			responseInfo.payload = http.getString(); 		//getting the repsonse from the get
+  		if (httpCode){ 	//If the POST call was successful
+      			responseInfo.payload = http.getString(); 		
       			responseInfo.responseCode = httpCode;
   		}else{
-      			responseInfo.payload = http.errorToString(httpCode).c_str(); //I think this is the error information??
-      			responseInfo.responseCode = httpCode; //should be negative or zero?
+      			responseInfo.payload = http.errorToString(httpCode);
+      			responseInfo.responseCode = httpCode;
   		}
     		http.end();
   	}
@@ -56,17 +57,16 @@ namespace hackPSU {
   	Response::POST(String url, String payload){
     		HTTPClient http;
     		http.begin(url); //http Begin call
-    		int httpCode = http.GET(); //GET call
-    		http.addHeader(headerInfo.header);
+
+    		int httpCode = http.POST(payload); //POST call sending the payload?
 
   		//httpCode will be negative (or zero) if there is an error
-  		if (httpCode > 0){ 	//If the GET call was successful
-      			responseInfo.payload = http.getString(); 		//getting the repsonse from the get
+  		if (httpCode){ 	//If the POST call was successful
+      			responseInfo.payload = http.getString(); 		
       			responseInfo.responseCode = httpCode;
   		}else{
-      			responseInfo.payload = http.errorToString(httpCode).c_str(); //I think this is the error information??
-      			responseInfo.responseCode = httpCode; //should be negative or zero?
+      			responseInfo.payload = http.errorToString(httpCode);
+      			responseInfo.responseCode = httpCode;
   		}
     		http.end();
-  	}	
-}
+  	}
