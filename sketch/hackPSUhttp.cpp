91baf8f08c1e5fbadfba8482a0efaed38a07677c
jmm8046 <35675433+jmm8046@users.noreply.github.com>
2018-08-04 23:20:48 -0400

    Update hackPSUhttp.cpp

--- sketch/hackPSUhttp.cpp
+++ sketch/hackPSUhttp.cpp
@@ -1,74 +1,76 @@
 //https://techtutorialsx.com/2016/07/21/esp8266-post-requests/
-#include <hackPSUhttp.h>
+#include "hackPSUhttp.h"
 
 namespace hackPSU {
-	Response::GET(String url, String payload, struct headers){
+	static Response* HTTP::GET(String url, int count, Headers headers[]){
 		HTTPClient http;
    		http.begin(url); //http Begin call
-    		http.addHeader(Headers.headerInformation);
+
+        for (int i = 0; i<count, i++){
+			Headers headers = headers[i];
+        	http.addHeader(headers.headerKey,headers.headerValue);
+        }
+
 		int httpCode = http.GET(); //GET call
 
+		Response* responseInfo;
+        responseInfo = new Response;
 
-  		//httpCode will be negative (or zero) if there is an error
+        responseInfo->payload = http.getString(); 		//getting the repsonse from the get
+        responseInfo->responseCode = httpCode;
 
-  		//HAVE IF FOR IF NEGATIVE AND ERROR CONNECTION TO WIFI??
-  		if (httpCode > 0){ 	//If the GET call was successful
-      			responseInfo.payload = http.getString(); 		//getting the repsonse from the get
-      			responseInfo.responseCode = httpCode;
-  		}else{
-     			responseInfo.payload = http.errorToString(httpCode); //I think this is the error information??
-     			responseInfo.responseCode = httpCode; //should be negative or zero?
-  		}
-    		http.end();
-  	}
+    	http.end();
+    	return responseInfo;
+	}
 
-  	Response::GET(String url, String payload){
-    		HTTPClient http;
-    		http.begin(url); //http Begin call
+  	static Response HTTP::GET(String url){
+    	HTTPClient http;
+    	http.begin(url); //http Begin call
    		int httpCode = http.GET(); //GET call
 
-  		//httpCode will be negative (or zero) if there is an error
-  		if (httpCode > 0){ 	//If the GET call was successful
-     			responseInfo.payload = http.getString(); 		//getting the repsonse from the get
-      			responseInfo.responseCode = httpCode;
-  		}else{
-      			responseInfo.payload = http.errorToString(httpCode); //I think this is the error information??
-      			responseInfo.responseCode = httpCode; //should be negative or zero?
-  		}
-    		http.end();
+   		Response* responseInfo;
+        responseInfo = new Response;
+
+     	responseInfo->payload = http.getString(); 		//getting the repsonse from the get
+        responseInfo->responseCode = httpCode;
+
+    	http.end();
+    	return responseInfo;
   	}
 
-  	Response::POST(String url, String payload, struct headers){
-    		HTTPClient http;
-    		http.begin(url); //http Begin call
-    		http.addHeader(Headers.headerInformation);
+  	static Response HTTP::POST(String url, String payload, int count, Headers headers[]){
+    	HTTPClient http;
+    	http.begin(url); //http Begin call
+    	
+        for (int i = 0; i<count, i++){
+			Headers headers = headers[i];
+			http.addHeader(headers.headerKey,headers.headerValue);
+         }
 
-    		int httpCode = http.POST(payload); //POST call sending the payload?
+    	int httpCode = http.POST(payload); //POST call sending the payload?
 
-  		//httpCode will be negative (or zero) if there is an error
-  		if (httpCode){ 	//If the POST call was successful
-      			responseInfo.payload = http.getString(); 		
-      			responseInfo.responseCode = httpCode;
-  		}else{
-      			responseInfo.payload = http.errorToString(httpCode);
-      			responseInfo.responseCode = httpCode;
-  		}
-    		http.end();
+    	Response* responseInfo;
+        responseInfo = new Response;
+
+        responseInfo->payload = http.getString(); 		
+        responseInfo->responseCode = httpCode;
+
+    	http.end();
+    	return responseInfo;
   	}
 
-  	Response::POST(String url, String payload){
-    		HTTPClient http;
-    		http.begin(url); //http Begin call
+  	static Response HTTP::POST(String url, String payload){
+    	HTTPClient http;
+    	http.begin(url); //http Begin call
 
-    		int httpCode = http.POST(payload); //POST call sending the payload?
+    	int httpCode = http.POST(payload); //POST call sending the payload?
 
-  		//httpCode will be negative (or zero) if there is an error
-  		if (httpCode){ 	//If the POST call was successful
-      			responseInfo.payload = http.getString(); 		
-      			responseInfo.responseCode = httpCode;
-  		}else{
-      			responseInfo.payload = http.errorToString(httpCode);
-      			responseInfo.responseCode = httpCode;
-  		}
-    		http.end();
+    	Response* responseInfo;
+        responseInfo = new Response;
+
+        responseInfo->payload = http.getString(); 		
+        responseInfo->responseCode = httpCode;
+
+    	http.end();
+    	return responseInfo;
   	}
