544185815e7b0eb999af04858370eb90996b54b5
jmm8046 <35675433+jmm8046@users.noreply.github.com>
2018-08-05 00:59:50 -0400

    Add more functions to HTTP wrapper (#5)
    Added HTTP library.

--- HackPSUlcd/HackPSUlcd.ino
+++ HackPSUlcd/HackPSUlcd.ino
@@ -1,6 +1,6 @@
 #include <LiquidCrystal_I2C.h>
 
-// Construct an LCD object and pass it the 
+// Construct an LCD object and pass it the
 // I2C address, width (in characters) and
 // height (in characters). Depending on the
 // Actual device, the IC2 address may change.
@@ -25,9 +25,67 @@ void setup() {
 
   // Move the cursor to the next line and print
   // WORLD.
-  lcd.setCursor(5, 1);      
+  lcd.setCursor(5, 1);
   lcd.print("WORLD");
 }
 
 void loop() {
+  printDebug("Food", 88);
+  delay(1000);
+  printName("Pranav");
+  delay(1000);
+  printSize("M");
+  delay(1000);
+  printScan(true);
+  delay(1000);
+  printLocation("Food");
+  delay(1000);
+  printRegistered("True");
+  delay(1000);
+  printMsg("HackPSU");
+  delay(1000);
+}
+
+void printDebug(String location, int wifi){
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Loc: " + location + ", " + wifi + "%");
+}
+
+void printName(String name){
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Hi " + name);
+}
+
+
+void printSize(String size){
+    lcd.clear();
+    lcd.setCursor(0, 1);
+    lcd.print("Size: " + size);
+}
+
+void printScan(bool isgoodScan){
+  lcd.clear();
+  lcd.setCursor(0, 1);
+  lcd.print("Allow in: " + isgoodScan);
+}
+
+void printLocation(String location){
+  lcd.clear();
+  lcd.setCursor(0, 1);
+  lcd.print("Location: " + location);
+}
+
+void printRegistered(String registered){
+  lcd.clear();
+  lcd.setCursor(0, 1);
+  lcd.print("Registered: " + registered);
+}
+
+
+void printMsg(String msg){
+  lcd.clear();
+  lcd.setCursor(0, 1);
+  lcd.print(msg);
 }
