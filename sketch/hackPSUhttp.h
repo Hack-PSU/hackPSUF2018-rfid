43782696019e763c97d06b59803fdbf845782f57
Matthew Heilman <mdh5389@psu.edu>
2018-09-02 18:59:47 -0400

    Working combined sketch

--- sketch/hackPSUhttp.h
+++ sketch/hackPSUhttp.h
@@ -1,30 +1,55 @@
 #pragma once 
 
-#include <malloc.h>
-#include <stdint.h>
-#include <stdlib.h>
-#include <HttpClient.h>
+#include <ESP8266HTTPClient.h>
+#include <ESP8266WiFi.h>
 
 namespace hackPSU {
   typedef struct Response {
-      String payload;
-      int responseCode;
-  }Response;   //declare structure obejct to return
+    String payload;
+    String errorMessage;
+    int responseCode;
+  } Response;   //declare structure obejct to return
 
-  typedef struct Headers{   //does this belong here??
-       String headerKey;
-       String headerValue;
-  }Headers;
-
+  typedef struct Headers{
+    String headerKey;
+    String headerValue;
+  } Headers;
+  
   class HTTP{
     public:
-    /**
-     * TODO: Add docstring explaining these parameters
+      /*
+        Use case: When a GET request must be made with headers
+        Parameters: String url (redis url), int count (amount of headers), Headers headers 
+        Returns: payload and response code
+        Response is allocated by the function and should be freed upon completion by the caller
      */
       static Response* GET(String url, int count, Headers headers[]);
+
+      /*
+        Use case: When a GET request must be made without headers
+        Parameters: String url (redis url)
+        Returns: payload and response code
+        Response is allocated by the function and should be freed upon completion by the caller
+     */
       static Response* GET(String url);
+
+      /*
+        Use case: When a POST request must be made with headers
+        Parameters: String url (redis url), String payload (content going to redis), int count (amount of headers), Headers headers 
+        Returns: payload and response code
+        Response is allocated by the function and should be freed upon completion by the caller
+     */
       static Response* POST(String url, String payload, int count, Headers headers[]);
+      
+      /*
+        Use case: When a POST request must be made without headers
+        Parameters: String url (redis url)
+        Returns: payload and response code
+        Response is allocated by the function and should be freed upon completion by the caller
+     */
       static Response* POST(String url, String payload);
+
+      static String handleError(int httpCode);
 
   };
 }
