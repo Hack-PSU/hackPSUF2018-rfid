4791ab86a6f266c70fece5b2a7cbd4f6b2f4a1eb
Matthew Heilman <mdh5389@psu.edu>
2018-09-09 21:44:30 -0400

    Added keypad library and continued building out demo sketch

--- sketch/hackPSUkeypad.cpp
+++ sketch/hackPSUkeypad.cpp
@@ -2,6 +2,107 @@
 #include "hackPSUkeypad.h"
 
 namespace hackPSU{
+  void Keypad::pulse(bool data, int space){
+    digitalWrite(sig, data ? HIGH : LOW);
+    digitalWrite(clk, HIGH);
+    delay(space / 2);
+    digitalWrite(clk, LOW);
+    delay(space / 2);
+  }
+
+  Keypad::Keypad(int KPD_SRC, int KPD_CLK, int KPD_SIG, void (*clear)(), void (*putChar)(char)){
+    pin = KPD_SRC;
+    clk = KPD_CLK;
+    sig = KPD_SIG;
+    pinMode(clk, OUTPUT);
+    pinMode(sig, OUTPUT);
+    pinMode(pin, INPUT);
+    clearSR();
+
+    this->clear = clear;
+    this->putChar = putChar;
+  }
+
+  char Keypad::readKeypad(){
+    // assumed that the shift registor has been cleared
+    char key = 'x'; // x is the default return value, only returned if no keypress is detected
+    int i = 0;
+  
+    for(int i = 0; i < 4 && key == 'x'; i++){
+      pulse(i == 0, READ_PERIOD);  
+      int tmp = analogRead(pin);
+      if( tmp > 800 ) key = keys[0][3-i];       // constants refers to analog value when key is pressed
+      else if( tmp > 450 ) key = keys[1][3-i];  // The actual value fluxuates minimally so there is room
+      else if( tmp > 320 ) key = keys[2][3-i];  // for this behavior
+      else if( tmp > 50)   key = keys[3][3-i];
+      //Serial.println(String(tmp) + " - " + key);
+    }
+    clearSR(); // reset shift registor after every read
+    return key;
+  }
+
+  #define SPACE 5
+  char Keypad::getKeyPress(int stable, unsigned long timeout) {
+    char val;
+    unsigned long start = millis();
+    
+    val = readKeypad();
+    delay(SPACE);
+    int i;
+    for( i = 0; i <= stable && start + timeout > millis(); i++) {
+      char temp = readKeypad();
+      if( i == stable && val != 'x') {
+        return(val);   
+      } else if( temp != val ) {
+        val = temp;
+        i = 0;
+      }  
+      delay(SPACE);
+    }
+    return('z');
+  }
+
+  char Keypad::getUniqueKey(unsigned long timeout){
+    unsigned long start = millis();
+    char val = getKeyPress(1, timeout);  // 500 ms timeout
+    Serial.println("Key pressed: " + val);
+    if(val != 'z'){
+      Serial.print(val);
+      while( getKeyPress(1, start + timeout - millis()) == val && start + timeout > millis() ) { // check every 50ms
+        Serial.print('.');
+        delay(50);
+      }
+      Serial.print('\n');
+    }
+    return(start + timeout < millis() ? 't' : val);
+  }
+
+  String Keypad::getPin(int maxLen, const char clr, const char submit, int timeout){
+    unsigned long start = millis();
+    String pin = "";
+    for(int i = 0; i < maxLen && start + timeout > millis(); i++){
+      char key = getUniqueKey(start + timeout - millis());
+      if(key == clr){
+        clear();
+        pin = "";
+        i = 0;
+      } else if( key == submit) {
+        return pin;
+      } else if( key != 't' && key != 'z') {
+        putChar(key);
+        pin += key;
+        start = millis(); // timeout is from the last pressed key
+      } else {
+        i--;
+      }
+    }
+    if( start + timeout < millis() ){
+      Serial.println("Timeout in getPin");
+      return "timeout";
+    } else {
+      return pin;
+    }
+  }
   
 }
 
--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -1,9 +1,14 @@
-//#include "hackPSUrfid.h"
+//#include <MD5.h>
+
+#include "hackPSUrfid.h"
 #include "hackPSUstatemachine.h"
+#include "hackPSUkeypad.h"
 #include <SPI.h>
 #include <MFRC522.h>
 #include <LiquidCrystal_I2C.h>
 using namespace hackPSU;
+
+void dump_byte_array(byte*, byte);
 
 #define clearSR() shiftOut(KPD_SIG, KPD_CLK, MSBFIRST, B00000000);
 
@@ -34,8 +39,20 @@ const byte I2C_ADDRESSES[] = {0x27, 0x3f};
 LiquidCrystal_I2C *lcd;
 MFRC522 mfrc522(RFID_SS, RFID_RST);  // Create MFRC522 instance
 
+void putChar(char character) {
+  lcd->print(character);
+  Serial.println("Character printed: " + character);
+}
+void clear(){
+  Serial.println("CLEAR SENT");
+  lcd->clear();
+  lcd->setCursor(1,0);
+}
+Keypad keypad(KPD_SRC, KPD_CLK, KPD_SIG, &clear, &putChar);
+
 //Scanner scanner(RFID_SS, RFID_RST);
 StateMachine fsm();
+
 
 void setup() {
 
@@ -43,18 +60,14 @@ void setup() {
   while(!Serial);
   Serial.println("Begin of program");
   
-  pinMode(KPD_CLK, OUTPUT);
-  pinMode(KPD_SIG, OUTPUT);
-  pinMode(KPD_SRC, INPUT);
-  clearSR();
 
   Wire.begin();
   for(byte i : I2C_ADDRESSES){
     Wire.beginTransmission(i);
     if(Wire.endTransmission() == 0){
       lcd = new LiquidCrystal_I2C(i, 16, 2);
-//      Serial.print("I2C LCD found at 0x" + i < 16 ? "0":"");
-//      Serial.println(i, HEX);
+      Serial.print("I2C LCD found at 0x");
+      Serial.println(i, HEX);
       break;
     }
   }
@@ -64,24 +77,25 @@ void setup() {
   lcd->backlight();
   lcd->setCursor(0,0);
   lcd->print("Hello!");
+  
   lcd->setCursor(0,1);
   lcd->print("Scan wristband");
-
   
   SPI.begin();      // Init SPI bus
   mfrc522.PCD_Init();   // Init MFRC522
   mfrc522.PCD_DumpVersionToSerial();  // Show details of PCD - MFRC522 Card Reader details
-  
-  
+    
 }
 
 void loop() {
   // Look for new cards
+  
   if ( ! mfrc522.PICC_IsNewCardPresent()) {
     delay(50);
     return;
+  // Select one of the cards  
   }
-  // Select one of the cards
+
   if ( ! mfrc522.PICC_ReadCardSerial()) {
     delay(50);
     return;
@@ -93,94 +107,31 @@ void loop() {
   dump_byte_array(mfrc522.uid.uidByte, mfrc522.uid.size);
   
   lcd->setCursor(0,1);
-  for(int i = 0; i < 8; i++){
-    char key = getUniqueKey(A0);
-    if(key != 'u') lcd->print(key);
-    else i--;
-  }
+  
+  String pin = keypad.getPin(10, '*','#', 15000);
+  Serial.println("PIN received: " + pin);
+  delay(500);
   lcd->clear();
   lcd->setCursor(0,0);
   lcd->print("Hello!");
+  
   lcd->setCursor(0,1);
   lcd->print("Scan wristband");
-}
-
-void changeState(){
   
 }
-/**
- * Input: Analog pin to read value
- */
-char readKeypad(int pin){
-  char key = 'x'; // x is the default return value, only returned if no keypress is detected
-  int i = 0;
 
-  for(int i = 0; i < 4 && key == 'x'; i++){
-    toggle(i == 0, 4);
-    int tmp = analogRead(pin);
-    if( tmp > 800 ) key = keys[0][3-i];
-    else if( tmp > 450 ) key = keys[1][3-i];
-    else if( tmp > 320 ) key = keys[2][3-i];
-    else if( tmp > 50)  key = keys[3][3-i];
-    /*
-    if( tmp > 800 ) key = keys[0][4-i];      // 1024
-    else if( tmp > 450 ) key = keys[1][4-i]; // 535, 440
-    else if( tmp > 320 ) key = keys[2][4-i]; // 355, 275
-    else if( tmp > 50 )  key = keys[3][4-i]; // 368, 200
-    */
-    //Serial.println(String(tmp) + " - " + key);
-  }
-  clearSR();
-  return key;
-}
-
-void toggle(bool data, int space) {
-  digitalWrite(KPD_SIG, data ? HIGH : LOW);
-  digitalWrite(KPD_CLK, HIGH);
-  delay(space / 2);
-  digitalWrite(KPD_CLK, LOW);
-  delay(space / 2);
-}
-
-char getUniqueKey(int pin = A0){
-  char val = getKeyPress(1, 500, pin);
-  if(val != 'u'){
-    Serial.print(val);
-    while(getKeyPress(1, 250, pin) == val) Serial.print('.');
-    Serial.print('\n');
-  }
-  return(val);
-}
-
-#define SPACE 5
-char getKeyPress(int stable = 3, long timeout = 1500, int pin = A0) {
-  char val;
-  unsigned long start = millis();
-  
-  val = readKeypad(pin);
-  delay(SPACE);
-  int i;
-  for( i = 0; i <= stable && start + timeout > millis(); i++) {
-    char temp = readKeypad(pin);
-    //if(temp != 'x') Serial.print(temp);
-    if( i == stable && val != 'x') {
-      //Serial.print("Time = " + String(millis() - start));
-      return(val);   
-    } else if( temp != val ) {
-      val = temp;
-      i = 0;
-    }  
-    delay(SPACE);
-  }
-  return('u');
-}
-
-
+//void changeState(){
+//  
+//}
 
 // Helper routine to dump a byte array as hex values to Serial
 void dump_byte_array(byte *buffer, byte bufferSize) {
+  lcd->print("0x");
   for (byte i = 0; i < bufferSize; i++) {
-    lcd->print(buffer[i] < 0x10 ? " 0" : " ");
+    if(buffer[i] < 0x10){
+      lcd->print("0");
+    }
     lcd->print(buffer[i], HEX);
   }
+
 }
