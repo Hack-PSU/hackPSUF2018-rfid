6655a90cb0f7c8eabd8c1e0b5117fad728d1f7e1
Tom <tommyzhao111@gmail.com>
2018-09-20 21:46:51 -0400

    Fixed header and cpp

---
+++ sketch/hackPSUdisplay.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <Arduino.h>
+#include <LiquidCrystal_I2C.h>
+
+namespace hackPSU {
+
+  typedef enum {PROD, DEV, HEADLESS} State_e;
+
+  class Display{
+    private:
+      State_e state;
+      LiquidCrystal_I2C* lcd;
+    public:
+      Display(State_e state);
+      void print(String msg);
+      void print(char msg);
+      void print(String msg, int row);
+      void clear();
+      void clear(int row);
+  };
+
+}
