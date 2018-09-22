7770ea8f2aeebcf21f3f51aeb489ca4e2d327050
David McDermott <davidsamuelmcdermott@gmail.com>
2018-09-22 15:34:11 -0400

    const -> constexpr for compile issue

--- sketch/hackPSUconfig.h
+++ sketch/hackPSUconfig.h
@@ -1,7 +1,10 @@
 #ifndef CONFIG__H
 #define CONFIG__H
 
-const char* REDIS  = "c-98-235-90-5.hsd1.pa.comcast.net";
-const char* SSID   = "psuwirelesssetup";
-const char* PASSWD = "";
+namespace hackPSU{
+  constexpr char* REDIS  = "c-98-235-90-5.hsd1.pa.comcast.net";
+  constexpr char* SSID   = "psuwirelesssetup";
+  constexpr char* PASSWD = "";
+}
+
 #endif
