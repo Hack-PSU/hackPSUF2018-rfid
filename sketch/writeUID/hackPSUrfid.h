4455490dcfb485b419e915acbf9378b475a83e1f
David McDermott <davidsamuelmcdermott@gmail.com>
2018-09-22 14:59:38 -0400

    Commmit before massive change

---
+++ sketch/writeUID/hackPSUrfid.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <stdint.h>
+#include <MFRC522.h>
+#include <SPI.h>
+//#include <MFRC522Hack.h>
+
+namespace hackPSU {
+
+  class Scanner{
+    private:
+      constexpr static byte DEFAULT_KEY[6] = {0xff, 0xff, 0xff, 0xff, 0xff, 0xff};
+      const byte* key;
+      MFRC522 reader;
+    public:
+      Scanner(uint8_t ssPin, uint8_t rstPin, const byte* key);
+      Scanner(uint8_t ssPin, uint8_t rstPin);
+      
+      uint32_t getUID(void);
+      void setUID(uint32_t uid);
+      
+  };
+
+}
