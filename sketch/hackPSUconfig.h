2b426dd5512963086efe93565271664459251907
David McDermott <davidsamuelmcdermott@gmail.com>
2018-09-23 23:36:43 -0400

    packaging for parallel edits

--- sketch/hackPSUconfig.h
+++ sketch/hackPSUconfig.h
@@ -7,6 +7,7 @@ namespace hackPSU{
   constexpr char* REDIS  = "c-98-235-90-5.hsd1.pa.comcast.net:3000";
   constexpr char* SSID   = "McDermott";
   constexpr char* PASSWD = "mathematical";
+  //TODO add fingerprint of redis cert
 
   typedef struct{
     String name;
