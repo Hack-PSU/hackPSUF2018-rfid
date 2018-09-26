fb49ec29c443fc78683319226dec88b7bf1a47c6
David McDermott <davidsamuelmcdermott@gmail.com>
2018-09-26 16:53:29 -0400

    removed more passwords

--- sketch/readWriteData/hackPSUconfig.h
+++ sketch/readWriteData/hackPSUconfig.h
@@ -3,8 +3,8 @@
 
 namespace hackPSU{
   constexpr char* REDIS  = "192.168.0.11:3000";//"c-98-235-90-5.hsd1.pa.comcast.net";
-  constexpr char* SSID   = "TheBlueWhat";
-  constexpr char* PASSWD = "The_Gr3@t-68";
+  constexpr char* SSID   = "";
+  constexpr char* PASSWD = "";
   constexpr char* MASTER_KEY = "GET_REKT";
 
   typedef struct{
