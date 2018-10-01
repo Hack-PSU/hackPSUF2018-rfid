76a35dda6423c9c583a33b472e32786d51890686
Tom <tommyzhao111@gmail.com>
2018-09-30 20:46:39 -0400

    Custom chars and basic scroll

---
+++ testLCD/testLCD.ino
@@ -0,0 +1,14 @@
+#include "hackPSUdisplay.h"
+using namespace hackPSU;
+
+Display *display;
+
+void setup() {
+  // put your setup code here, to run once:
+  display = new Display(DEV);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+    
+}
